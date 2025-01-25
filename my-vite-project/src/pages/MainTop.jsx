f337954429413cfca8320d5863af8e114bdc87f5
ENES ŞEN <infoenessen@gmail.com>
2025-01-25 15:16:24 +0300

    add react-icons and implement carousel in MainTop component; enhance styles and layout

--- my-vite-project/src/pages/MainTop.jsx
+++ my-vite-project/src/pages/MainTop.jsx
@@ -3,13 +3,24 @@ import Avatar from "@mui/material/Avatar";
 import Stack from "@mui/material/Stack";
 import { Button } from "@mui/material";
 import { deepOrange } from "@mui/material/colors";
+import Carousel from "react-bootstrap/Carousel";
+import MainTopImage1 from "../assets/main-top-image-1.png";
+import MainTopImage2 from "../assets/main-top-image-2.png";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa"; // FontAwesome iconları
 
 const MainTop = () => {
   return (
     <div className='main-top-container'>
-      <Stack direction='row' spacing={3}>
+      <Stack
+        direction='row'
+        spacing={2}
+        sx={{
+          alignItems: "flex-start",
+          justifyContent: "flex-start",
+          marginLeft: "-20px"
+        }}>
         <Avatar
-          sx={{ bgcolor: deepOrange[500] }}
+          sx={{ bgcolor: deepOrange[500], width: 70, height: 70 }}
           alt='Enes Şen'
           src='/static/images/avatar/1.jpg'
         />
@@ -29,6 +40,35 @@ const MainTop = () => {
           </p>
         </Stack>
       </Stack>
+
+      <Carousel
+        className='carousel-container'
+        nextIcon={<FaChevronRight className='carousel-button-icon' />}
+        prevIcon={<FaChevronLeft className='carousel-button-icon' />}>
+        <Carousel.Item>
+          <img src={MainTopImage1} alt='First slide' />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img src={MainTopImage2} alt='Second slide' />
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img src={MainTopImage1} alt='Third slide' />
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
     </div>
   );
 };
